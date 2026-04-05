504ca6cb08b9576e3acb7db16ccb5ead2e71d573
artur.bardzinski <artur.bardzinski@icloud.com>
2026-04-05 08:01:48 +0200

    refactor: streamline login tests by integrating SecurePage and updating assertions

--- src/tests/login.pw.test.ts
+++ src/tests/login.pw.test.ts
@@ -1,21 +1,18 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { LoginPage } from "../pages/login.page.js";
+import { SecurePage } from "../pages/secure.page.js";
 
 test("Login: happy path (valid credentials)", async ({ page }) => {
   const loginPage = new LoginPage(page);
+  const securePage = new SecurePage(page);
 
   await loginPage.open();
 
-  // Dane są podane na stronie jako poprawne:
-  // username: tomsmith
-  // password: SuperSecretPassword!
-  // (źródło: opis strony)  [oai_citation:1‡the-internet.herokuapp.com](https://the-internet.herokuapp.com/login?utm_source=chatgpt.com)
+  // Dane demo ze strony (the-internet.herokuapp.com): tomsmith / SuperSecretPassword!
   await loginPage.login("tomsmith", "SuperSecretPassword!");
 
-  await loginPage.expectSuccess();
-
-  // Dodatkowa asercja: po zalogowaniu URL zawiera /secure
-  await expect(page).toHaveURL(/\/secure/);
+  await securePage.expectLoaded();
+  await securePage.expectLoginSuccessMessage();
 });
 
 test("Login: negative (invalid username)", async ({ page }) => {
